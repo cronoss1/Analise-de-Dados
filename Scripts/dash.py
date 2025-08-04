7ed0e100ee35f302820ddd06fe96e5da8ecc241f
cronoss1 <kauanlopes282@gmail.com>
2025-08-03 22:42:28 -0300

    Testes

--- Scripts/dash.py
+++ Scripts/dash.py
@@ -2,4 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as sl
 
+df = pd.read_csv(r"c:\Users\Cronoss\Documents\Dashboards\Data\vgsales.csv") # load the Dataset
+df[["Rank", "Name"]] # Select the columns Rank and Name
+
 
